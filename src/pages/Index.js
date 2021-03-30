4379b8273a58c6065b95bc97b3529d27c7448556
issyzac <issyzac.iz@gmail.com>
2021-03-31 00:44:31 +0300

    Added an index page

---
+++ src/pages/Index.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+import IndexContents from '../components/IndexContents'
+
+class Index extends Component {
+    render(){
+        return(
+            <IndexContents />
+        )
+    }
+}
+
+export default Index;
