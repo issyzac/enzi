7d536db556653b6b0adf09a4139ad9537757b782
issyzac <issyzac.iz@gmail.com>
2021-03-31 01:28:25 +0300

    Import font awesome icons once

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Index from './pages/Index';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
 
 function App() {
   return (
