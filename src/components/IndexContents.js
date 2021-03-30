2963e146f4fbf68bbcd7578a9c5c33177b1bcd71
issyzac <issyzac.iz@gmail.com>
2021-03-31 00:44:13 +0300

    Added an index page contents component

---
+++ src/components/IndexContents.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import SiteHeader from './HeaderComponent'
+
+class IndexContents extends Component {
+    render(){
+        return(
+            <div>
+                <SiteHeader />
+            </div>
+        );
+    }
+}
+
+export default IndexContents;
