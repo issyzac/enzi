043092c0f562b04fc551c3a38b4b999be90ff6ee
issyzac <issyzac.iz@gmail.com>
2021-03-31 01:33:24 +0300

    Updated menu item

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -19,7 +19,7 @@ function SiteHeader() {
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#link">About</Nav.Link>
             </Nav>
             
             <FontAwesomeIcon icon="glass-cheers" />
