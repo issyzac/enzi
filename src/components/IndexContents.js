89a4dade70321ba423cfcd7b8c46db10be1bde03
issyzac <issyzac.iz@gmail.com>
2021-03-31 12:55:12 +0300

    Linked gadgets section to the home page

--- src/components/IndexContents.js
+++ src/components/IndexContents.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import BannerComponent from './BannerComponent'
 import BlendsComponent from './BlendsComponent'
+import GadgetComponent from './GadgetsComponent'
 import SiteHeader from './HeaderComponent'
 
 class IndexContents extends Component {
@@ -10,6 +11,7 @@ class IndexContents extends Component {
                 <SiteHeader />
                 <BannerComponent />
                 <BlendsComponent />
+                <GadgetComponent />
             </div>
         );
     }
