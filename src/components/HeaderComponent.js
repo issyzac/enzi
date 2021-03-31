368ea336e4a3c66a99e9518f64425fa24c330476
issyzac <issyzac.iz@gmail.com>
2021-03-31 03:33:39 +0300

    Added logo to the app bar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,7 @@
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import {Navbar, Nav, Card} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'react-bootstrap/Image'
+import logo from '../images/logo.png'
 
 const navbarstyle = {
     height: '80px',
@@ -8,24 +10,43 @@ const navbarstyle = {
 }
 
 const brandStyle = {
-    color: "#202a44"
+    color: "#202a44",
+    fontSize: '20px',
+    marginLeft: '50px',
+    marginRight: '50px'
+}
+
+const logoStyle = {
+    height: '60px',
+    widht: '60px'
 }
 
 function SiteHeader() {
     return (
-        <Navbar bg="light" expand="lg" style={navbarstyle}>
-        <Navbar.Brand href="#home" style={brandStyle}>Enzi Coffee</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">About</Nav.Link>
-            </Nav>
-            
-            <FontAwesomeIcon icon="glass-cheers" />
+        <Card>
+            <Navbar bg="light" expand="lg" style={navbarstyle}>
+            <Navbar.Brand href="#home" style={brandStyle} className="d-flex flex-column justify-content-center">
+                <div className="row">
+                    <div className="col">
+                        <Image src={logo} fluid style={logoStyle} />
+                    </div>
+                    <div className="col d-flex flex-column justify-content-center" style={{ lineHeight: '0.9', paddingLeft: '0' }}>
+                        <p style={{ margin: '0' }}> Enzi Coffee <br/> Roasters </p>
+                    </div>
+                </div>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">About</Nav.Link>
+                </Nav>
+                
+                <FontAwesomeIcon icon="glass-cheers" />
 
-        </Navbar.Collapse>
-        </Navbar>
+            </Navbar.Collapse>
+            </Navbar>
+        </Card>
     );
 }
 
