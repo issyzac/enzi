6bbae5eb514d7ef6d6cb2936f4895e64b31c349e
issyzac <issyzac.iz@gmail.com>
2021-03-31 04:11:45 +0300

    Created blends component

---
+++ src/components/BlendsComponent.js
@@ -0,0 +1,15 @@
+
+const blendStyle = {
+    height: '500px',
+    backgroundColor: '#fff0e6'
+}
+
+function BlendsComponent(){ 
+    return (
+        <div style={blendStyle}>
+
+        </div>
+    )
+}
+
+export default BlendsComponent;
