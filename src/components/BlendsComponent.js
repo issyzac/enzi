95d2ea6665b0cd9e264aeb27cee94562c429100a
issyzac <issyzac.iz@gmail.com>
2021-03-31 15:39:35 +0300

    updated coffee blend card

--- src/components/BlendsComponent.js
+++ src/components/BlendsComponent.js
@@ -32,67 +32,25 @@ function BlendsComponent(){
     return (
         <div className="container-fluid" style={blendStyle}>
             <div className="row" style={{ marginLeft: '7rem', marginRight: '7rem' }}>
-                <div className="col-md-4">
-                    <Card style={blendCardSyle}>
-                        <Card.Img variant="top" src={blendPlaceHolder2} style={{ width: '20rem', alignSelf: 'center' }}/>
-                        <Card.Body>
-                            <div className="" style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start' }}>
-                                <h5> Enzi Classic Mbeya <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span> </h5> 
-                                <br />
-                                <div className="row" style={{ fontFamily: 'Nunito', fontWeight: '300', fontSize: '0.7em', textAlign: 'start' }}>
-                                   <div className="col">
-                                   <p> 
-                                        <b style={subtitleStyle}> Region </b> <br /> 
-                                        <span> Mbeya </span> 
-                                    </p> 
-                                   </div>
-                                   <div className="col">
-                                    <p> 
-                                        <b style={subtitleStyle}>Notes</b> <br /> 
-                                        <span> Citrus, Chocolate, Orange Peel, Sweet </span> 
-                                    </p>
-                                   </div>
-                                </div>
-                                <div style={{ fontFamily: 'Nunito', fontWeight: '300', fontSize: '0.7em', textAlign: 'start' }}>
-                                    <p> 
-                                        <b style={subtitleStyle}>Dubbed</b> <br /> 
-                                        <span> Sweetest Taboo! </span> 
-                                    </p>
-                                </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
+                <div class="col-lg-4 col-md-6 col-sm-6" style={blendCardSyle}>
+                    <div class="blog__item">
+                        <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem' }}>
+                            <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span>
+                                <span><img src={blendPlaceHolder2} alt="" /> 16 February 2020</span>
+                                <h5 style={{ fontFamily: 'Nunito' }}> Enzi Classic Mbeya </h5>
+                            <a href="index.html#">Order Now</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-4 align-content-center">
-                    <Card style={blendCardSyle}>
-                        <Card.Img variant="top" src={blendPlaceHolder2} style={{ width: '20rem', alignSelf: 'center' }}/>
-                        <Card.Body>
-                        <div className="" style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start' }}>
-                                <h5> Enzi Kili  <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span> </h5> 
-                                <br />
-                                <div className="row" style={{ fontFamily: 'Nunito', fontWeight: '300', fontSize: '0.7em', textAlign: 'start' }}>
-                                   <div className="col">
-                                   <p> 
-                                        <b style={subtitleStyle}> Region </b> <br /> 
-                                        <span> Kilimanjaro </span> 
-                                    </p> 
-                                   </div>
-                                   <div className="col">
-                                    <p> 
-                                        <b style={subtitleStyle}>Notes</b> <br /> 
-                                        <span> Citrus, Chocolate, Orange Peel, Sweet </span> 
-                                    </p>
-                                   </div>
-                                </div>
-                                <div style={{ fontFamily: 'Nunito', fontWeight: '300', fontSize: '0.7em', textAlign: 'start' }}>
-                                    <p> 
-                                        <b style={subtitleStyle}>Dubbed</b> <br /> 
-                                        <span> Everybody is a morning person! </span> 
-                                    </p>
-                                </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
+                <div class="col-lg-4 col-md-6 col-sm-6" style={blendCardSyle}>
+                    <div class="blog__item">
+                        <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem' }}>
+                            <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span>
+                                <span><img src={blendPlaceHolder2} alt="" /> 16 February 2020</span>
+                                <h5 style={{ fontFamily: 'Nunito' }}> Enzi Kili </h5>
+                            <a href="index.html#">Order Now</a>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-md-4 d-flex flex-column justify-content-center" style={{ padding: '3em' }}>
                     <h1 style={{ fontFamily: 'Nunito' }}>
