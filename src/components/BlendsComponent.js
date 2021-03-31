e0e7b8d600cfd4f8cc42cc1f3a3cfa420e3d6d77
issyzac <issyzac.iz@gmail.com>
2021-03-31 04:43:40 +0300

    add images to components

--- src/components/BlendsComponent.js
+++ src/components/BlendsComponent.js
@@ -1,13 +1,44 @@
+import {Card} from 'react-bootstrap'
+import enziClassicBlendImg from '../images/blends/ec001.png'
+import enziClassicBlendImg2 from '../images/blends/ec002.jpg'
 
 const blendStyle = {
-    height: '500px',
-    backgroundColor: '#fff0e6'
+    backgroundColor: '#fff0e6' //fff0e6 e8f5ff
+}
+
+const blendCardSyle = {
+    marginTop: '5rem',
+    marginBottom: '5rem'
 }
 
 function BlendsComponent(){ 
     return (
-        <div style={blendStyle}>
-
+        <div className="container-fluid" style={blendStyle}>
+            <div className="row" style={{ marginLeft: '7rem', marginRight: '7rem' }}>
+                <div className="col-md-4">
+                    <Card style={blendCardSyle}>
+                        <Card.Img variant="top" src={enziClassicBlendImg} />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4 align-content-center">
+                    <Card style={blendCardSyle}>
+                        <Card.Img variant="top" src={enziClassicBlendImg2} />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4"></div>
+            </div>
         </div>
     )
 }
