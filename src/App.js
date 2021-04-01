553756b1a1ebcd5751d49cf855256b41eb25ac0e
issyzac <issyzac.iz@gmail.com>
2021-04-02 00:31:35 +0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Index from './pages/Index';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Scrollbar} from 'smooth-scrollbar-react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
