805eeda4a41eac01a371ecd19d7237c300bb76e6
issyzac <issyzac.iz@gmail.com>
2021-04-12 03:09:12 +0300

    Updated coffee blends graphics section

--- src/components/BlendsComponent.js
+++ src/components/BlendsComponent.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 //Images
 import enziClassicBlendImg2 from '../images/blends/ec002-0.png'
-import blendPlaceHolder1 from '../images/blends/blend-placeholder.png'
+import blendPlaceHolder from '../images/blends/blend-mokup-straight.jpeg'
 import blendPlaceHolder2 from '../images/blends/blend-placeholder-two.png'
 
 
@@ -14,7 +14,7 @@ const blendStyle = {
 
 const blendCardSyle = {
     marginTop: '5rem',
-    marginBottom: '5rem'
+    marginBottom: '5rem',
 }
 
 const flagsStyle = {
@@ -51,9 +51,9 @@ function BlendsComponent(){
             <div className="row" style={{ marginLeft: '7rem', marginRight: '7rem' }}>
                 <div class="col-lg-4 col-md-6 col-sm-6" style={blendCardSyle}>
                     <div class="blog__item">
-                        <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem' }}>
+                        <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem', backgroundColor: '#f7f7f7' }}>
                             <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span>
-                                <span><img src={blendPlaceHolder2} alt="" style={{ padding: '4rem' }} /> Enzi Mbeya</span>
+                                <span><img src={blendPlaceHolder} alt="" /> Enzi Mbeya</span>
                                 <h5 style={{ fontFamily: 'Nunito' }}> Enzi Mbeya </h5>
                             <a href="index.html#">Order Now</a>
                         </div>
@@ -61,9 +61,9 @@ function BlendsComponent(){
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6" style={blendCardSyle}>
                     <div class="blog__item">
-                        <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem' }}>
+                        <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem', backgroundColor: '#f7f7f7' }}>
                             <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span>
-                                <span><img src={blendPlaceHolder2} alt="" style={{ padding: '4rem' }} /> Enzi Kili</span>
+                                <span><img src={blendPlaceHolder} alt=""/> Enzi Kili</span>
                                 <h5 style={{ fontFamily: 'Nunito' }}> Enzi Kili </h5>
                             <a href="index.html#">Order Now</a>
                         </div>
