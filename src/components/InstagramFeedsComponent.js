b5ccd980310127438e049d78b59e93e9dc722405
issyzac <issyzac.iz@gmail.com>
2021-04-17 15:32:08 +0300

    refactor

--- src/components/InstagramFeedsComponent.js
+++ src/components/InstagramFeedsComponent.js
@@ -1,60 +1,61 @@
 import InstagramEmbed from 'react-instagram-embed';
 import './styles/InstagramEmbedStyles.css'
 
-const embededPostStyle = {
-    paddind: '5rem'
-}
-
 function InstagramFeedsComponent(){
     return(
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <div style={embededPostStyle}>
-                        <InstagramEmbed
-                            url='https://www.instagram.com/p/CNBJ7yLDSkH/'
-                            clientAccessToken='460297381974247|65bce5f9f05540a62db19a1515839948'
-                            hideCaption={true}
-                            maxWidth={320}
-                            containerTagName='div'
-                            injectScript
-                            onLoading={() => {}}
-                            onSuccess={() => {}}
-                            onAfterRender={() => {}}
-                            onFailure={() => {}}
-                        />
-                    </div>
-                </div>
-                <div className="col">
-                    <div style={embededPostStyle}>
-                         <InstagramEmbed
-                            url='https://www.instagram.com/p/CMSbFSXDGQ5/'
-                            clientAccessToken='460297381974247|65bce5f9f05540a62db19a1515839948'
-                            hideCaption={true}
-                            maxWidth={320}
-                            containerTagName='div'
-                            injectScript
-                            onLoading={() => {}}
-                            onSuccess={() => {}}
-                            onAfterRender={() => {}}
-                            onFailure={() => {}}
-                        />
-                    </div>
-                </div>
-                <div className="col">
-                    <div style={embededPostStyle}>
-                         <InstagramEmbed
-                            url='https://www.instagram.com/p/CL6VIXrDOWn/'
-                            clientAccessToken='460297381974247|65bce5f9f05540a62db19a1515839948'
-                            hideCaption={true}
-                            maxWidth={320}
-                            containerTagName='div'
-                            injectScript
-                            onLoading={() => {}}
-                            onSuccess={() => {}}
-                            onAfterRender={() => {}}
-                            onFailure={() => {}}
-                        />
+        <div className="container-fluid" style={{ backgroundColor: '#bb906b', paddingTop: '5rem', paddingBottom: '5rem' }}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-9">
+                        <div className="col-md-3">
+                            <div style={{  }}>
+                                <InstagramEmbed
+                                    url='https://www.instagram.com/p/CNBJ7yLDSkH/'
+                                    clientAccessToken='460297381974247|65bce5f9f05540a62db19a1515839948'
+                                    hideCaption={true}
+                                    maxWidth={150}
+                                    containerTagName='div'
+                                    injectScript
+                                    onLoading={() => {}}
+                                    onSuccess={() => {}}
+                                    onAfterRender={() => {}}
+                                    onFailure={() => {}}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div style={{  }}>
+                                <InstagramEmbed
+                                    url='https://www.instagram.com/p/CMSbFSXDGQ5/'
+                                    clientAccessToken='460297381974247|65bce5f9f05540a62db19a1515839948'
+                                    hideCaption={true}
+                                    maxWidth={150}
+                                    containerTagName='div'
+                                    injectScript
+                                    onLoading={() => {}}
+                                    onSuccess={() => {}}
+                                    onAfterRender={() => {}}
+                                    onFailure={() => {}}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div style={{  }}>
+                                <InstagramEmbed
+                                    url='https://www.instagram.com/p/CL6VIXrDOWn/'
+                                    clientAccessToken='460297381974247|65bce5f9f05540a62db19a1515839948'
+                                    hideCaption={true}
+                                    maxWidth={150}
+                                    containerTagName='div'
+                                    injectScript
+                                    onLoading={() => {}}
+                                    onSuccess={() => {}}
+                                    onAfterRender={() => {}}
+                                    onFailure={() => {}}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
