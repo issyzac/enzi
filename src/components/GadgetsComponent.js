47cca373b24b924765349dcd2bfe8e42def1325e
issyzac <issyzac.iz@gmail.com>
2021-04-19 19:53:35 +0300

    Updated add to cart to Buy Now

--- src/components/GadgetsComponent.js
+++ src/components/GadgetsComponent.js
@@ -53,7 +53,7 @@ function GadgetComponent() {
                             <Card.Body>
                                 <Card.Title style={gadgetTextStyle}>Aeropress</Card.Title>
                                 <Card.Title style={gadgetTextPriceStyle}>120,000 TSH</Card.Title>
-                                <Button variant='outline-dark' style={addtocartButtonStyle} > Add to Cart </Button>
+                                <Button variant='outline-dark' style={addtocartButtonStyle} > Buy Now </Button>
                             </Card.Body>
                         </Card>
                     </div>
@@ -63,7 +63,7 @@ function GadgetComponent() {
                             <Card.Body>
                                 <Card.Title style={gadgetTextStyle}>Hand Grinder</Card.Title>
                                 <Card.Title style={gadgetTextPriceStyle}>100,000 TSH</Card.Title>
-                                <Button variant='outline-dark' style={addtocartButtonStyle} > Add to Cart </Button>
+                                <Button variant='outline-dark' style={addtocartButtonStyle} > Buy Now </Button>
                             </Card.Body>
                         </Card>
                     </div>
@@ -73,7 +73,7 @@ function GadgetComponent() {
                             <Card.Body>
                                 <Card.Title style={gadgetTextStyle}>Moka Pot</Card.Title>
                                 <Card.Title style={gadgetTextPriceStyle}>140,000 TSH</Card.Title>
-                                <Button variant='outline-dark' style={addtocartButtonStyle} > Add to Cart </Button>
+                                <Button variant='outline-dark' style={addtocartButtonStyle} > Buy Now </Button>
                             </Card.Body>
                         </Card>
                     </div> */}
@@ -83,7 +83,7 @@ function GadgetComponent() {
                             <Card.Body>
                                 <Card.Title style={gadgetTextStyle}>Electric Grinder</Card.Title>
                                 <Card.Title style={gadgetTextPriceStyle}>200,000 TSH</Card.Title>
-                                <Button variant='outline-dark' style={addtocartButtonStyle} > Add to Cart </Button>
+                                <Button variant='outline-dark' style={addtocartButtonStyle} > Buy Now </Button>
                             </Card.Body>
                         </Card>
                     </div>
