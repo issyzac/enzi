1d90ba6a6d9140c49488fde546e130a3aaf69a1a
issyzac <issyzac.iz@gmail.com>
2021-04-19 19:28:45 +0300

    Mobile optimisation

--- src/components/BannerComponent.js
+++ src/components/BannerComponent.js
@@ -4,8 +4,8 @@ import Flags from 'country-flag-icons/react/3x2'
 import blendMockupImage from '../images/blends/blend-mokup-slant.jpeg'
 
 const bannerStyle = {
-    paddingTop: '150px',
-    paddingBottom: '150px',
+    paddingTop: '50px',
+    paddingBottom: '100px',
     backgroundColor: '#f7f7f7'
 }
 
@@ -27,12 +27,17 @@ function BannerComponent(){
     return(
         <div id="#home" className="container" style={bannerStyle}>
             <div className="row">
-                <div className="col-md-6 col-sm-12 d-flex flex-column justify-content-center">
+
+                <div className="col-md-6 col-sm-12 d-md-none d-lg-none" style={{ marginBottom: "5rems" }}>
+                    <Image src={blendMockupImage} fluid />
+                </div>
+
+                <div className="col-md-6 col-sm-12 d-flex flex-column justify-content-center" style={{ marginTop: '5rem' }}>
                     <div className="">
                         <h4 style={bannerTextStyle}> Coffee, Kahawa, Kaffee, Kofe, Café, Kopi</h4>
                     </div>
                     <div className="">
-                        <p style={{ textAlign: 'start', fontSize: '20px', fontFamily: 'poppins', fontWeight: '300' }}> 
+                        <p style={{ textAlign: 'start', fontSize: '20px', fontFamily: 'poppins', fontWeight: '300', marginTop: '3rems' }}> 
                             At <b style={{ backgroundColor: '#202a44', paddingLeft: '5px', color: '#ffffff', marginRight: '5px' }}> enzi </b>  we love coffee and everything that is involved with it, we always go an
                             extra mile to make sure every sip of a cup is a unique experience for our customers </p>
                     </div>
@@ -45,10 +50,11 @@ function BannerComponent(){
                     </div>
                 </div>
                 <div className="d-md-none col-sm-12" style={{ height: '7rem'}}></div>
-                <div className="col-md-6 col-sm-12" style={{ marginTop: "5rems" }}>
+                <div className="col-md-6 col-sm-12 d-md-block d-sm-none d-none" style={{ marginTop: "5rems" }}>
                     <Image src={blendMockupImage} fluid />
                 </div>
             </div>
+            <div className="row d-none" style={{ height: '120px' }}></div>
         </div>
     )
 }
