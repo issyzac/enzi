0dcfc3e7c4e7a05a614994619b7a9ed33fa02ac3
issyzac <issyzac.iz@gmail.com>
2021-05-20 15:26:05 +0300

    Added subscription page

---
+++ src/pages/SubscriptionPage.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import SiteHeader from '../components/HeaderComponent'
+
+class SubscriptionPage extends Component {
+    render(){
+        return(
+            <div>
+                <SiteHeader />
+            </div>
+        )
+    }
+}
+
+export default SubscriptionPage;
