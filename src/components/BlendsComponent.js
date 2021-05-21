85008ccbf0006073c4fc56ba0a6f8f4113174bf9
issyzac <issyzac.iz@gmail.com>
2021-05-21 22:47:15 +0300

    refactored blends component to add gadgets slider

--- src/components/BlendsComponent.js
+++ src/components/BlendsComponent.js
@@ -1,11 +1,18 @@
 import {Card, Button} from 'react-bootstrap'
 import Flags from 'country-flag-icons/react/3x2'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import './styles/enziStyles.css'
 //Images
 import enziClassicBlendImg2 from '../images/blends/ec002-0.png'
 import blendPlaceHolder from '../images/blends/blend-mokup-straight.jpeg'
 import blendPlaceHolder2 from '../images/blends/blend-placeholder-two.png'
+
+import { Carousel } from 'react-bootstrap'
+
+import moka from '../images/gadgets/moka-pot.jpg'
+import grinder from '../images/gadgets/handgrinder.jpeg'
+import electricGrinder from '../images/gadgets/electric-grinder.jpg'
+import aeropress from '../images/gadgets/aeropress.jpg'
 
 
 const blendStyle = {
@@ -52,31 +59,49 @@ function BlendsComponent(){
                 <div className="col-md-1"></div>
                 <div className="col-md-10">
                     <div className="row">
-                        <div class="col-md-4 col-sm-12" style={blendCardSyle}>
-                            <div class="blog__item">
-                                <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem', backgroundColor: '#f7f7f7' }}>
-                                    <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span>
-                                        <span><img src={blendPlaceHolder} alt="" /> Enzi</span>
-                                        <h5 style={{ fontFamily: 'Nunito' }}> Enzi </h5>
-                                    <a href="https://wa.me/255683321768">Order Now</a>
-                                </div>
-                            </div>
+                        <div class="col-md-6 col-sm-12" style={blendCardSyle}>
+                            <Carousel style={{ padding: '50px' }}>
+                                <Carousel.Item>
+                                    <img
+                                    className="d-block w-100"
+                                    src={moka}
+                                    alt="First slide"
+                                    />
+                                    <Carousel.Caption>
+
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+
+                                <Carousel.Item>
+                                    <img
+                                    className="d-block w-100"
+                                    src={grinder}
+                                    alt="Second slide"
+                                    />
+
+                                    <Carousel.Caption>
+                                    
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+
+                                <Carousel.Item>
+                                    <img
+                                    className="d-block w-100"
+                                    src={aeropress}
+                                    alt="Third slide"
+                                    />
+
+                                    <Carousel.Caption>
+                                    
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                </Carousel>
                         </div>
-                        <div class="col-md-4 col-sm-12" style={blendCardSyle}>
-                            <div class="blog__item">
-                                <div class="blog__item__text"  style={{ fontFamily: 'Nunito', fontWeight: '700', fontSize: '1.5em', textAlign: 'start', paddingBottom: '3rem', backgroundColor: '#f7f7f7' }}>
-                                    <span> <Flags.TZ title="Tanzania" style={flagsStyle}/> </span>
-                                        <span><img src={blendPlaceHolder} alt=""/> Enzi Lite</span>
-                                        <h5 style={{ fontFamily: 'Nunito' }}> Enzi Lite </h5>
-                                    <a href="https://wa.me/255683321768">Order Now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-12 d-flex flex-column justify-content-center" style={{ padding: '3em' }}>
-                            <h1 style={{ fontFamily: 'Nunito', color: '#ffffff', fontWeight: '700' }}>
-                                Explore our variety of blends made perfect for every moment
+                        <div className="col-md-6 col-sm-12 d-flex flex-column justify-content-center" style={{ padding: '3em' }}>
+                            <h1 style={{ fontSize: '3rem', fontFamily: 'Poppins', color: '#ffffff', fontWeight: '700' }}>
+                                Good deals on gadgets at our coffeeware shop
                             </h1>
-                            <Button variant='outline-light' href="https://wa.me/255683321768" style={enziButtonStyle} > Shop Enzi </Button>
+                            <Button variant='outline-light' href="https://wa.me/255683321768" style={enziButtonStyle} > Shop Now </Button>
                         </div>
                     </div>
                 </div>
