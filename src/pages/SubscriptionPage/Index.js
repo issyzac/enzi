d531c4bcd4c39b46ff1695ba5d09ca8699704538
issyzac <issyzac.iz@gmail.com>
2021-05-26 15:25:55 +0300

    Switch page component based on the route matched by router

--- src/pages/SubscriptionPage/Index.js
+++ src/pages/SubscriptionPage/Index.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import { useRouteMatch, Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SiteHeader from '../../components/HeaderComponent'
 import HeaderComponent from './components/Header'
 import ChooseGadgetComponent from "./components/ChooseGadget";
@@ -6,21 +7,35 @@ import ChooseTexture from './components/ChooseTexture';
 import ChooseBlend from './components/ChooseBlend';
 import ChooseAmount from './components/ChooseAmount';
 
-class SubscriptionPage extends Component {
-    render(){
+function SubscriptionPage(){
+
+        let match = useRouteMatch();
+
         return(
             <div>
                 <SiteHeader />
                 <div style={{ paddingTop: '80px', backgroundColor: "#ffffff" }}>
-                    <HeaderComponent />
-                    <ChooseGadgetComponent />
-                    <ChooseTexture />
-                    <ChooseBlend />
-                    <ChooseAmount />
+                    <Switch>
+                        <Route exact path={`${match.path}/`}>
+                            <HeaderComponent />
+                        </Route>
+                        <Route path={`${match.path}/gadgets`}>
+                            <ChooseGadgetComponent url={match.path} />
+                        </Route>
+                        <Route path={`${match.path}/texture`}>
+                            <ChooseTexture />
+                        </Route>
+                        <Route path={`${match.path}/gadgets`}>
+                            <ChooseBlend />
+                        </Route>
+                        <Route path={`${match.path}/gadgets`}>
+                            <ChooseAmount />
+                        </Route>
+                    </Switch>
                 </div>
             </div>
         )
-    }
 }
+
 
 export default SubscriptionPage;
