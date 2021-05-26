3b611d7b779dafe1028724e7d35cb517c8cd3c69
issyzac <issyzac.iz@gmail.com>
2021-05-27 02:45:03 +0300

    Added link to choosing the blend

--- src/pages/SubscriptionPage/components/ChooseBlend.js
+++ src/pages/SubscriptionPage/components/ChooseBlend.js
@@ -1,11 +1,12 @@
 import { Button, Image } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 import blendMockupImage from '../../../images/blends/blend-mokup-straight.jpeg'
 import blendMockupImageSlant from '../../../images/blends/blend-mokup-slant.jpeg'
 
 import '../styles/SubStyle.css';
 
-function ChooseBlend(){
+function ChooseBlend({url}){
     return(
         <div className="container-fluid" style={{ paddingTop: '7rem', paddingBottom: '5rem' }}> 
             <div className="container">
@@ -15,16 +16,18 @@ function ChooseBlend(){
                     </p>
                     <div className="row">
                         <div className="col-md-6 col-sm-12"> 
-                            <div className='sub-select-blend-btn' >
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <Image src={blendMockupImage} fluid />
-                                    </div>
-                                    <div className="col-md-6" style={{ textAlign: 'center', height: '100%', marginTop: '3rem', marginBottom: '3rem' }}>
-                                        <span className="sub-select-blend-btn-text"> Enzi </span>
-                                    </div>
-                                </div>    
-                            </div> 
+                            <Link to={`${url}/amount`} id="enzi-link">
+                                <div className='sub-select-blend-btn' >
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <Image src={blendMockupImage} fluid />
+                                        </div>
+                                        <div className="col-md-6" style={{ textAlign: 'center', height: '100%', marginTop: '3rem', marginBottom: '3rem' }}>
+                                            <span className="sub-select-blend-btn-text"> Enzi </span>
+                                        </div>
+                                    </div>    
+                                </div> 
+                            </Link>
                         </div>
                         <div className="col-md-6 col-sm-12"> 
                             <div className='sub-select-blend-btn'>
