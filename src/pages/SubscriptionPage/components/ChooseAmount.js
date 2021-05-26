e272f67d9765fd237a11a99da326b705b3404419
issyzac <issyzac.iz@gmail.com>
2021-05-27 00:22:38 +0300

    refactored: standardised selection buttons

--- src/pages/SubscriptionPage/components/ChooseAmount.js
+++ src/pages/SubscriptionPage/components/ChooseAmount.js
@@ -1,43 +1,44 @@
 import { Component } from 'react';
 import { Button } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
-const enziButtonStyle = {
-    border: '1px solid #202a44',
-    color: '#202a44',
-    alignSelf: 'start',
-    marginBottom: '1rem',
-    marginRight: '1rem',
-    fontVariant: 'all-small-caps',
-    fontFamily: 'Nunito',
-    fontWeight: '700',
-    fontSize: '1.2rem',
-    borderRadius: '2px',
-    paddingLeft: '3rem',
-    paddingRight: '3rem',
-    paddingTop: '3rem',
-    paddingBottom: '3rem',
-    width: '100%',
-  }
+import '../styles/SubStyle.css';
 
-function ChooseAmount () {
+function ChooseAmount ({url}) {
     return(
         <div className="container-fluid" style={{ paddingTop: '7rem', paddingBottom: '5rem' }}>
             <div className="container">
                 <div style={{ marginBottom: '50px'}}>
-                    <p className="title" style={{ fontFamily: 'Inter', fontWeight: '900', color: '#202a44', fontSize: '72px', textAlign: 'start', lineHeight: '5rem'}}> 
+                    <p className="title" style={{ marginBottom: '3rem', fontFamily: 'Inter', fontWeight: '900', color: '#202a44', fontSize: '72px', textAlign: 'start', lineHeight: '5rem'}}> 
                         How many bags per delivery? </p>
                         <div className="row">
                             <div className="col-md-6 col-sm-12"> 
-                                <Button variant='outline-dark' href="https://wa.me/255683321768" style={enziButtonStyle} > 1 Bag </Button> 
+                                <Link to={`${url}/contact`} id="enzi-link" >
+                                    <div className="sub-select-btn" > 
+                                        1 Bag (500 grams) 
+                                    </div>
+                                </Link>
                             </div>
                             <div className="col-md-6 col-sm-12"> 
-                                <Button variant='outline-dark' href="https://wa.me/255683321768" style={enziButtonStyle} > 2 Bags </Button> 
+                                <Link to={`${url}/contact`} id="enzi-link">
+                                    <div className="sub-select-btn" > 
+                                        2 Bags (1 Kg) 
+                                    </div> 
+                                </Link>
                             </div>
                             <div className="col-md-6 col-sm-12"> 
-                                <Button variant='outline-dark' href="https://wa.me/255683321768" style={enziButtonStyle} > 3 Bags </Button> 
+                                <Link to={`${url}/contact`} id="enzi-link">
+                                    <div className="sub-select-btn" > 
+                                        3 Bags (1.5 Kg) 
+                                    </div> 
+                                </Link>
                             </div>
                             <div className="col-md-6 col-sm-12"> 
-                                <Button variant='outline-dark' href="https://wa.me/255683321768" style={enziButtonStyle} > More Bags </Button> 
+                                <Link to={`${url}/contact`} id="enzi-link">
+                                    <div className="sub-select-btn" > 
+                                        Other 
+                                    </div> 
+                                </Link>
                             </div>
                         </div>
                 </div>
