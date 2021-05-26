1184d480f12826997614142e36a50734011c4ebb
issyzac <issyzac.iz@gmail.com>
2021-05-26 17:31:19 +0300

    cleanup

--- src/pages/SubscriptionPage/components/ChooseGadget.js
+++ src/pages/SubscriptionPage/components/ChooseGadget.js
@@ -11,31 +11,6 @@ import v60 from '../res/v60.png';
 import chemex from '../res/chemex.png';
 import esspressoMachine from '../res/esspresso-machine.png';
 
-const enziButtonStyle = {
-    border: '1px solid #202a44',
-    color: '#202a44',
-    alignSelf: 'start',
-    marginBottom: '1rem',
-    marginRight: '1rem',
-    fontVariant: 'all-small-caps',
-    fontFamily: 'Nunito',
-    fontWeight: '700',
-    fontSize: '1.2rem',
-    borderRadius: '2px',
-    paddingLeft: '3rem',
-    paddingRight: '3rem',
-    paddingTop: '3rem',
-    paddingBottom: '3rem',
-    width: '100%',
-  }
-
-  const textStyle = {
-    fontFamily: 'Poppins', 
-    fontWeight: '500', 
-    alignSelf: 'center', 
-    fontSize: '22px'
-  }
-
 function ChooseGadgetComponent({url}){
 
     let match = useRouteMatch();
@@ -44,7 +19,7 @@ function ChooseGadgetComponent({url}){
         <div className="container-fluid" style={{ paddingTop: '7rem', paddingBottom: '5rem' }}>
             <div className="container">
                 <div style={{ marginBottom: '50px'}}>
-                    <p className="title" style={{ fontFamily: 'Inter', fontWeight: '900', color: '#202a44', fontSize: '72px', textAlign: 'start', lineHeight: '5rem'}}> 
+                    <p className="title" style={{ marginBottom: '3rem', fontFamily: 'Inter', fontWeight: '900', color: '#202a44', fontSize: '72px', textAlign: 'start', lineHeight: '5rem'}}> 
                         How do you usually make your coffee? </p>
                         <div className="row">  
                             <GadgetItem name="Aeropress" icon={aeropress} url={url} />
