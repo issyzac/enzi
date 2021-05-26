27bb44910bbdd039dda75abf614755fdee40ef58
issyzac <issyzac.iz@gmail.com>
2021-05-26 17:11:41 +0300

    Refactor: standardize selection buttons

--- src/pages/SubscriptionPage/components/ChooseTexture.js
+++ src/pages/SubscriptionPage/components/ChooseTexture.js
@@ -1,25 +1,10 @@
 import { Component } from 'react';
 import { Button } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
-const enziButtonStyle = {
-    border: '1px solid #202a44',
-    color: '#202a44',
-    alignSelf: 'start',
-    marginBottom: '1rem',
-    marginRight: '1rem',
-    fontVariant: 'all-small-caps',
-    fontFamily: 'Nunito',
-    fontWeight: '700',
-    fontSize: '1.2rem',
-    borderRadius: '2px',
-    paddingLeft: '3rem',
-    paddingRight: '3rem',
-    paddingTop: '3rem',
-    paddingBottom: '3rem',
-    width: '100%',
-  }
+import "../styles/SubStyle.css";
 
-function ChooseTexture(){
+function ChooseTexture({url}){
     return(
         <div className="container-fluid" style={{ paddingTop: '7rem', paddingBottom: '5rem' }}>
             <div className="container">
@@ -29,10 +14,18 @@ function ChooseTexture(){
                     </p>
                         <div className="row">
                             <div className="col-md-6 col-sm-12"> 
-                                <Button variant='outline-dark' href="https://wa.me/255683321768" style={enziButtonStyle} > Ground Coffee </Button> 
+                                <Link to={`${url}/blend`} id="enzi-link">
+                                    <div className="sub-select-btn" > 
+                                        <span className="sub-select-btn-text"> Ground Coffee </span>
+                                    </div> 
+                                </Link>
                             </div>
                             <div className="col-md-6 col-sm-12"> 
-                                <Button variant='outline-dark' href="https://wa.me/255683321768" style={enziButtonStyle} > Whole Beans </Button> 
+                            <Link to={`${url}/blend`} id="enzi-link">
+                                    <div className="sub-select-btn" >
+                                        <span className="sub-select-btn-text"> Whole Beans </span>
+                                    </div> 
+                                </Link>
                             </div>
                         </div>
                 </div>
