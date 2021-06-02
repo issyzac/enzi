cbd7300613d68d10e5e44573f69de41258b7aa7b
issyzac <issyzac.iz@gmail.com>
2021-06-02 23:52:29 +0300

    Refactored: Standardized components

--- src/pages/SubscriptionPage/components/ChooseAmount.js
+++ src/pages/SubscriptionPage/components/ChooseAmount.js
@@ -12,37 +12,25 @@ function ChooseAmount ({url}) {
                     <p className="title" style={{ marginBottom: '3rem', fontFamily: 'Inter', fontWeight: '900', color: '#202a44', fontSize: '72px', textAlign: 'start', lineHeight: '5rem'}}> 
                         How many bags per delivery? </p>
                         <div className="row">
-                            <div className="col-md-6 col-sm-12"> 
-                                <Link to={`${url}/contact`} id="enzi-link" >
-                                    <div className="sub-select-btn" > 
-                                        1 Bag (500 grams) 
-                                    </div>
-                                </Link>
-                            </div>
-                            <div className="col-md-6 col-sm-12"> 
-                                <Link to={`${url}/contact`} id="enzi-link">
-                                    <div className="sub-select-btn" > 
-                                        2 Bags (1 Kg) 
-                                    </div> 
-                                </Link>
-                            </div>
-                            <div className="col-md-6 col-sm-12"> 
-                                <Link to={`${url}/contact`} id="enzi-link">
-                                    <div className="sub-select-btn" > 
-                                        3 Bags (1.5 Kg) 
-                                    </div> 
-                                </Link>
-                            </div>
-                            <div className="col-md-6 col-sm-12"> 
-                                <Link to={`${url}/contact`} id="enzi-link">
-                                    <div className="sub-select-btn" > 
-                                        Other 
-                                    </div> 
-                                </Link>
-                            </div>
+                            <AmountItem title="2 Bags (1 Kg)" url={url} />
+                            <AmountItem title="4 Bags (2 Kg)" url={url} />
+                            <AmountItem title="6 Bags (3 Kg)" url={url} />
+                            <AmountItem title="Other" url={url} />
                         </div>
                 </div>
             </div>
+        </div>
+    )
+}
+
+function  AmountItem({title, url}) {
+    return (
+        <div className="col-md-6 col-sm-12"> 
+            <Link to={`${url}/contact`} id="enzi-link">
+                <div className="sub-select-btn" style={{ paddingTop: '2rem', paddingBottom: '3rem' }} > 
+                    {title}
+                </div> 
+            </Link>
         </div>
     )
 }
