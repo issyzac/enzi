1a45aa0211c566cf3ae97fbfc275967063e33491
issyzac <issyzac.iz@gmail.com>
2021-06-02 23:51:34 +0300

    Switch page content based on the URL matched

--- src/pages/SubscriptionPage/Index.js
+++ src/pages/SubscriptionPage/Index.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+
 import { useRouteMatch, Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SiteHeader from '../../components/HeaderComponent'
 import HeaderComponent from './components/Header'
@@ -8,38 +9,35 @@ import ChooseBlend from './components/ChooseBlend';
 import ChooseAmount from './components/ChooseAmount';
 import ContactInfo from './components/ContactInfo';
 
-function SubscriptionPage(){
+export default function SubscriptionPage(){
 
-        let match = useRouteMatch();
+    let match = useRouteMatch();
 
-        return(
-            <div>
-                <SiteHeader />
-                <div style={{ paddingTop: '80px', backgroundColor: "#ffffff" }}>
-                    <Switch>
-                        <Route exact path={`${match.path}/`}>
-                            <HeaderComponent />
-                        </Route>
-                        <Route path={`${match.path}/gadgets`}>
-                            <ChooseGadgetComponent url={match.path} />
-                        </Route>
-                        <Route path={`${match.path}/texture`}>
-                            <ChooseTexture url={match.path} />
-                        </Route>
-                        <Route path={`${match.path}/blend`}>
-                            <ChooseBlend url={match.path} />
-                        </Route>
-                        <Route path={`${match.path}/amount`}>
-                            <ChooseAmount url={match.path} />
-                        </Route>
-                        <Route path={`${match.path}/contact`}>
-                            <ContactInfo url={match.path} />
-                        </Route>
-                    </Switch>
-                </div>
+    return(
+        <div>
+            <SiteHeader />
+            <div style={{ paddingTop: '80px', backgroundColor: "#ffffff" }}>
+                <Switch>
+                    <Route exact path={`${match.path}/`}>
+                        <HeaderComponent />
+                    </Route>
+                    <Route path={`${match.path}/gadgets`}>
+                        <ChooseGadgetComponent url={match.path} />
+                    </Route>
+                    <Route path={`${match.path}/texture`}>
+                        <ChooseTexture url={match.path} />
+                    </Route>
+                    <Route path={`${match.path}/blend`}>
+                        <ChooseBlend url={match.path} />
+                    </Route>
+                    <Route path={`${match.path}/amount`}>
+                        <ChooseAmount url={match.path} />
+                    </Route>
+                    <Route path={`${match.path}/contact`}>
+                        <ContactInfo url={match.path} />
+                    </Route>
+                </Switch>
             </div>
-        )
+        </div>    
+    )
 }
-
-
-export default SubscriptionPage;
