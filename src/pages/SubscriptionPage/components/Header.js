0f74129389b6731f7a77e67aa0c334f6cd726b78
issyzac <issyzac.iz@gmail.com>
2021-06-04 01:56:04 +0300

    Capture user beggining the subscription process

--- src/pages/SubscriptionPage/components/Header.js
+++ src/pages/SubscriptionPage/components/Header.js
@@ -1,6 +1,15 @@
 import { Button } from "react-bootstrap";
-import { Link, useRouteMatch } from 'react-router-dom';
+import { Link, Redirect, useRouteMatch } from 'react-router-dom';
 import '../../../components/styles/enziStyles.css';
+
+import '../../../services/FirebaseService.js';
+import firebase from '../../../Firestore.js'; 
+
+import Cookies from 'js-cookie';
+
+import React, { useState } from 'react';
+import { useUserReference } from "../../../contexts/SubscriptionContext";
+
 
 const enziButtonStyle = {
     border: '1px solid #202a44',
@@ -19,11 +28,35 @@ const enziButtonStyle = {
     paddingBottom: '1rem'
   }
 
+const urCookie = Cookies.get('user-reference-cookie');
+
 function HeaderComponent(){
+
+    const userReference = useUserReference();
+
+    console.log("USEREF: loading header component, value : "+userReference);
+
+    const [proceedToGadgets, setProceedToGadgets] = useState(false);
+
+    const db = firebase.firestore();
 
     let match = useRouteMatch();
 
+    const getStartedWithSubscription = () => {
+
+        console.log("USEREF: In another component => "+userReference);
+
+        if ((urCookie != null || urCookie != undefined) || userReference != null) {
+            console.log("Called before the button inside if statement");
+            const userId = urCookie == null ? userReference : urCookie;
+            const docRef = db.collection("users").doc(userId);
+            docRef.update({"gotStarted": "True"});
+            setProceedToGadgets(true);
+        }
+    }
+
     return(
+        proceedToGadgets ? <Redirect to={`${match.url}/gadgets`} /> : 
         <div className="container-fluid" style={{ paddingTop: '7rem', paddingBottom: '5rem' }}>
             <div className="container">
                 <div style={{ marginBottom: '50px'}}>
@@ -31,9 +64,7 @@ function HeaderComponent(){
                     <p className="subtitle" style={{ fontFamily: 'Spartan', fontWeight: '300', marginTop: '5rem', fontSize: '40px', textAlign: 'start', lineHeight: '3rem' }}> 
                         Your favorite coffee exactly how you want it, every first week of the month! </p>
                     <div style={{ marginTop: '5rem', textAlign: 'start' }}>
-                        <Link to={`${match.url}/gadgets`}>
-                            <Button variant='outline-dark' style={enziButtonStyle} > Get Started </Button>
-                        </Link>
+                        <Button onClick={getStartedWithSubscription} variant='outline-dark' style={enziButtonStyle} > Get Started </Button>
                     </div>
                 </div>
             </div>
