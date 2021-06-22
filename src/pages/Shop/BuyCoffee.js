682e4df5d70aefd0bc8dc6e02f4283703bab8778
issyzac <issyzac.iz@gmail.com>
2021-06-22 13:33:43 +0300

    Added basic components for buying coffee screen

--- src/pages/Shop/BuyCoffee.js
+++ src/pages/Shop/BuyCoffee.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
+
+import blendMockupImage from '../../images/blends/blend-mokup-straight.jpeg'
 
 function BuyCoffee(){
 
-return (
-    <h1> We are buying coffee now </h1>
-)
+    function BlendSection(){
+        return(
+            <div className="container-fluid" style={{ paddingLeft: '0px' }}> 
+            <h3 style={{ textAlign: 'start', fontFamily: 'Poppins', fontSize: '20px', fontWeight: '700' }}> Blend </h3>
+                <div className="row">
+                    <div className="col-md-6">
+                    </div> 
+                    <div className="col-md-6">
+                    </div> 
+                </div>
+            </div>
+        )
+    }
 
+    return (
+        <div className="container-fluid" style={{ backgroundColor: '#f5f5f5f5', paddingTop: '1rem'}}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-7" style={{ height: '500px'}}>
+                        <Image src={blendMockupImage} fluid />
+                    </div>
+                    <div className="col-md-5">
+                        <h2 style={{ fontFamily: 'Poppins', fontWeight: '900', marginTop: '3rem', fontSize: '3rem', textAlign: 'start' }}>
+                            Freshly Roasted Coffee
+                        </h2>
+                        <h4 style={{ textAlign: 'start', fontSize: '1rem', fontFamily: 'Poppins', marginTop: '1rem', marginBottom: '1rem', fontWeight: '200', }}>
+                            <b> 10,000/= <span style={{ marginLeft: '10px', marginRight: '10px', color: '#202a44' }}> | </span> 250grams</b>
+                        </h4>
+                        <BlendSection />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default BuyCoffee
