cd34617d55a8e31db270a9db3cea5d7d199a77b3
issyzac <issyzac.iz@gmail.com>
2021-06-22 14:51:34 +0300

    Added blend UI and selection implementation

--- src/pages/Shop/BuyCoffee.js
+++ src/pages/Shop/BuyCoffee.js
@@ -1,23 +1,43 @@
-import React from 'react'
-import { Image } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Image } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 import blendMockupImage from '../../images/blends/blend-mokup-straight.jpeg'
 
+const blendStyle = {
+    border: "1px solid lightgrey",
+    marginLeft: '1px',
+    marginRight: '1px',
+    height: '130px',
+    borderRadius: '10px'
+}
+
+const enziButtonStyle = {
+    // border: '0.2px solid #202a44',
+    border: '0.2px solid lightgrey',
+    color: '#202a44',
+    fontVariant: 'all-small-caps',
+    width: '100%',
+    height: '100%',
+    fontFamily: 'Nunito',
+    fontWeight: '700',
+    fontSize: '1.2rem',
+    borderRadius: '5px',
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+    paddingTop: '0.5rem',
+    paddingBottom: '0.5rem'
+  }
+
+  const enziButtonStyleSelected = {
+      ...enziButtonStyle,
+      border: "1px solid 202a44"
+  }
+
 function BuyCoffee(){
 
-    function BlendSection(){
-        return(
-            <div className="container-fluid" style={{ paddingLeft: '0px' }}> 
-            <h3 style={{ textAlign: 'start', fontFamily: 'Poppins', fontSize: '20px', fontWeight: '700' }}> Blend </h3>
-                <div className="row">
-                    <div className="col-md-6">
-                    </div> 
-                    <div className="col-md-6">
-                    </div> 
-                </div>
-            </div>
-        )
-    }
+    const [blendSelection, setBlendSelection] = useState(0)
+    const [amount, setAmount] = useState(1)
 
     function HeaderSection(){
         return(
@@ -33,6 +53,62 @@ function BuyCoffee(){
         )
     }
 
+    function BlendSection(){
+
+        function BlendItem({ blendCode, name }){
+
+            function handleBlendSelection(e){
+                setBlendSelection(blendCode)
+            }
+
+            return(
+                <div className="col-md-6" style={{ padding: '0px' }}>
+                    <Button onClick={handleBlendSelection} variant='outline-dark' style={ blendSelection == blendCode ? enziButtonStyleSelected : enziButtonStyle }>
+                        {name}
+                    </Button>
+                </div>
+            )
+        }
+
+        return(
+            <div className="container-fluid" style={{ paddingLeft: '0px', marginTop: '2rem' }}> 
+            <h3 style={{ textAlign: 'start', fontFamily: 'Poppins', fontSize: '16px', fontWeight: '200' }}> Blend </h3>
+                <div className="row" style={blendStyle}>
+                    <BlendItem blendCode={0} name="Enzi" />
+                    <BlendItem blendCode={1} name="Enzi Lite" />
+                </div>
+            </div>
+        )
+    }
+
+    function QuantitySelection(){
+        return(
+            <div className="container-fluid" style={{ paddingLeft: '0px', paddingRight: '0px', marginTop: '2rem' }}> 
+            <h3 style={{ textAlign: 'start', fontFamily: 'Poppins', fontSize: '16px', fontWeight: '200' }}> Quantity </h3>
+                <div className="row" style={blendStyle}>
+                    <div className="col-md-6" style={{ padding: '0px' }}>
+                        <Button variant='outline-dark' style={enziButtonStyle}>
+                            1 Bag
+                        </Button>
+                    </div> 
+                    <div className="col-md-6" style={{ padding: '0px' }}>
+                        <Button variant='outline-dark' style={enziButtonStyle}>
+                            3 Bags
+                        </Button>
+                    </div> 
+                </div>
+            </div>
+        )
+    }
+
+    function CheckoutButton(){
+        return(
+            <Link  to="/gadgetsware" style={{ textDecoration: 'none', width: '250px', marginTop: '2rem'}}>
+                <div id="shop-gadget-button" className="shop-gadget-button"> Checkout </div>
+            </Link>
+        )
+    }
+
     return (
         <div className="container-fluid" style={{ backgroundColor: '#f5f5f5f5', paddingTop: '1rem'}}>
             <div className="container">
@@ -43,6 +119,9 @@ function BuyCoffee(){
                     <div className="col-md-5">
                         <HeaderSection />
                         <BlendSection />
+                        <QuantitySelection />
+                        <div style={{ height: '50px' }}></div>
+                        <CheckoutButton />
                     </div>
                 </div>
             </div>
