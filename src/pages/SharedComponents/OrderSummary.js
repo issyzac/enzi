27f2f259841b73faa473bfd5fa7cdcb38328bab6
issyzac <issyzac.iz@gmail.com>
2021-06-25 15:12:56 +0300

    Oder Summary Component

---
+++ src/pages/SharedComponents/OrderSummary.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/**
+ * TODO: Stub out order summary section from checkout pages to be added here
+ */
+export default function OrderSummary(){
+    return(
+        <></>
+    )
+}
