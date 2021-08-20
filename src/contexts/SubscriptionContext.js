a2890366a14ef9424f9c1f03fe7a4907349776a0
issyzac <issyzac.iz@gmail.com>
2021-08-20 17:25:13 +0300

    Included shop reference inside user object

--- src/contexts/SubscriptionContext.js
+++ src/contexts/SubscriptionContext.js
@@ -31,6 +31,7 @@ export function SubscriptionProvider({ children }) {
  
     const usr = {
         userReference: '',
+        shopReference: '',
         subscriptionReference: '',
         fullName: '',
         email: '',
